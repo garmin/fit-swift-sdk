b17c94e1bda40ac05520870446544a5a25dc69b0
Jenkins <avn.hudson@garmin.com>
2026-01-08 10:18:38 -0600

    Garmin FIT SDK 21.188.0

--- Tests/FITSwiftSDKTests/DeveloperFieldTests.swift
+++ Tests/FITSwiftSDKTests/DeveloperFieldTests.swift
@@ -102,7 +102,7 @@ func createTestDeveloperField(developerDataIndex: UInt8 = 0, fieldDefinitionNumb
     @Test("Developer field equatable comparison", arguments: [
         .init(title: "Identical Dev Field", fieldFactory: { try createTestDeveloperField() }, expected: true),
         .init(title: "Dev Field with Different Name", fieldFactory: { try createTestDeveloperField(fieldName: "Field1") }, expected: false),
-        .init(title: "Dev Field with Different Field num", fieldFactory: { try try createTestDeveloperField(fieldDefinitionNumber: 1) }, expected: false),
+        .init(title: "Dev Field with Different Field num", fieldFactory: { try createTestDeveloperField(fieldDefinitionNumber: 1) }, expected: false),
         .init(title: "Dev Field with Different Type", fieldFactory: { try createTestDeveloperField(fitBaseType: FitBaseType.uint8) }, expected: false),
         .init(title: "Dev Field with Different Scale", fieldFactory:  { try createTestDeveloperField(scale: 10) }, expected: false),
         .init(title: "Dev Field with Different Offset", fieldFactory: { try createTestDeveloperField(offset: 100) }, expected: false),
